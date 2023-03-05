2f421ce6c5e54cff1670376b8fc41f5f69b6c200
natalialashko <natasha0909761805@gmail.com>
2023-03-05 11:17:40 +0200

    gelleri 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const galleryContainer = document.querySelector('.gallery');
+function createGalleryCardsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+    </div>
+    `;
+    }).join('');
+};
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 console.log(galleryItems);
+galleryContainer.addEventListener('click', onClick);
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    // console.log(event.target);
+    // const urlImgOriginal = event.target.dataset.source;
+    // console.log(urlImgOriginal);
+
+    const instance = basicLightbox.create(`
+            <img
+        src="${event.target.dataset.source}"
+        width="900"
+         height="700"
+         />
+    
+`)
+   instance.show()
+    }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
+const galleryContainer = document.querySelector('.gallery');
+function createGalleryCardsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    
+        <a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+  
+    `;
+    }).join('');
+};
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 console.log(galleryItems);
+galleryContainer.addEventListener('click', onClick);
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    
+    const lightbox = new SimpleLightbox('.gallery a', { caption: true, captionDelay: 250});
+  
+}
+
